4dabdb23b9111856b0c1bffd0b3332fab3b2c886
dasuttles <dasuttles@valdosta.edu>
2015-03-20 14:46:18 -0400

    20150320
    Starting on Video

--- DatabaseSearchPages/includes/ec_connect.php
+++ DatabaseSearchPages/includes/ec_connect.php
@@ -2,9 +2,8 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_DatabaseServer = "impa.valdosta.edu";
-$database_DatabaseServer = "extra_credit";
-$username_DatabaseServer = "archives_web";
-$password_DatabaseServer = "uhCs4fQpr";
-$DatabaseServer = mysql_pconnect($hostname_DatabaseServer, $username_DatabaseServer, $password_DatabaseServer) or trigger_error(mysql_error(),E_USER_ERROR); 
+$dbhost = "impa.valdosta.edu";
+$dbname = "extra_credit";
+$dbuser = "archives";
+$dbpassword = "tqBXsZwR2QnL2qFY";
 ?>
